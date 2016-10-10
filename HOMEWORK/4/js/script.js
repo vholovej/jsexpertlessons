cc7f2efc3f746d2e050b6584848f32ef09408828
Vasyl Holovej <vholovej@gmail.com>
2016-10-10 23:10:00 +0300

    Add files via upload

---
+++ HOMEWORK/4/js/script.js
@@ -0,0 +1,17 @@
+﻿/*В коде одна ошибка, будем ее искать */
+(function(win){
+	var params = 
+		{states:{
+			url:'/',
+			template: 'temlate.js'
+		}
+	};
+    function setStates(params){
+		if(win && !win.params){
+			win.params = params;
+		}
+	}
+	setStates();
+	console.log(params.states.template);
+}
+)(window);
